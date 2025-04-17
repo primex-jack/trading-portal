e7644b21461ad431b21f2e7a877922ae1d268716
Primex Jack <primex.jack@gmail.com>
2025-04-17 21:38:38 +0000

    Add missing /api/{bot}/account route to web.php to fix 404 error

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,37 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Api\TradeController;
 use App\Http\Controllers\Api\BotController;
 
-// Authentication routes
-Auth::routes();
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-// Dashboard routes (protected by auth)
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard/{bot}', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/overview', [DashboardController::class, 'overview'])->name('overview');
-    Route::get('/', function () {
-        return redirect()->route('overview');
-    });
+Route::get('/', function () {
+    $bots = array_keys(config('bots'));
+    return view('overview', compact('bots'));
 });
 
-// API routes (protected by auth)
-Route::middleware(['auth'])->prefix('api')->group(function () {
-    Route::get('/{bot}/trades/active', [TradeController::class, 'activeTrades']);
-    Route::get('/{bot}/trades/history', [TradeController::class, 'tradeHistory']);
-    Route::get('/{bot}/status', [BotController::class, 'status']);
-    Route::get('/trades/active', [TradeController::class, 'overviewActiveTrades']);
+Route::get('/dashboard/{bot}', function ($bot) {
+    $bots = array_keys(config('bots'));
+    if (!in_array($bot, $bots)) {
+        abort(404);
+    }
+    return view('dashboard', compact('bots', 'bot'));
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('api')->group(function () {
+    Route::get('{bot}/status', [BotController::class, 'status']);
+    Route::get('{bot}/trades/active', [TradeController::class, 'activeTrades']);
+    Route::get('{bot}/trades/history', [TradeController::class, 'tradeHistory']);
+    Route::get('trades/active', [TradeController::class, 'overviewActiveTrades']);
+    Route::get('{bot}/account', [TradeController::class, 'accountDetails']);
+});
